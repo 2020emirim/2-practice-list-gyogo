5d7d691526f70496f300ffe70c026d68ded8fe3e
Gyogo <dfva1234@naver.com>
2020-05-14 09:53:57 +0900

    views BookmarkList

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from bookmark.views import BookmarkList
+
 app_name = 'bookmark'
 
 urlpatterns = [
     path('', BookmarkList.as_view(), name='list'),  # bookmark:list
+
 ]
