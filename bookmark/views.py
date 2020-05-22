71f25c8ec20d7268a070ceed5bda3e18e11cefd2
Gyogo <dfva1234@naver.com>
2020-05-22 14:58:16 +0900

    views BookmarkUpdateView

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from bookmark.models import Bookmark
 
@@ -18,3 +18,10 @@ class BookmarkCreateView(CreateView):  # bookmark_form.html 지정된 이름
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
+
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name','url'] #<form>
+    template_name_suffix =  '_update' #bookmark_update.html
+    #success_url = reverse_lazy('boo')
